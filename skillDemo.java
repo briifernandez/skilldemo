63e311fa4a1e56e4b78536de0cc130afd1d3756f
Briana Fernandez <98505287+briifernandez@users.noreply.github.com>
2022-05-01 21:41:31 -0700

    Updates!!

--- skillDemo.java
+++ skillDemo.java
@@ -1,3 +1,7 @@
 public class skillDemo {
 
+    public static int multiply(int x, int y) {
+        return (x * y);
+    }
+
 }
---
+++ skillDemoTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+
+    @Test
+    public void multiply() {
+  
+        assertEquals(6, skillDemo.multiply(1, 2));
+  
+    }
+ 
+    
+}
+
